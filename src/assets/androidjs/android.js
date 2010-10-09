be7123eb59beb9cb7f110063e01c3dae773e389d
brianleroux <brian@westcoastlogic.com>
2010-10-09 16:23:59 -0700

    removed narwhal and moved android.js into androidjs folder

--- src/assets/androidjs/android.js
+++ src/assets/androidjs/android.js
@@ -1,11 +1,11 @@
 // evaluates a JavaScript asset into the current context
 var include = function(id) {
 	// FIXME check for exports, if it exists wrap and iterate over keys in current ctx
+	// FIXME ...everything appears to be root rel
     Activity.evalAssetFile(id + '.js');
 }
 
 // start the bootstrapping the bare minimum
-include('androidjs/lifecycle');
 include('androidjs/console');
 // -------------------------------------------------------------- //
 // 						Load up app.js!							  //
