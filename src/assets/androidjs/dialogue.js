e9847ee2a9316c1b28339081a3be3a08cb0f7ee6
brianleroux <brian@westcoastlogic.com>
2010-10-11 19:29:58 -0700

    dialogues: good enough for a .1 release

--- src/assets/androidjs/dialogue.js
+++ src/assets/androidjs/dialogue.js
@@ -27,6 +27,7 @@ exports.select = function(title, options, callback) {
 	dialog.show();
 }
 
+// XXX not sure we need this considering we have select; maybe select and multiselect are the only two needed?
 exports.selectOne = function(title, options, callback) {
 	var	dialog = new Packages.android.app.AlertDialog.Builder(Activity)
 	dialog.setTitle(title);
@@ -35,37 +36,57 @@ exports.selectOne = function(title, options, callback) {
 }
 
 // FIXME need to support complete, cancel
-exports.selectMany = function(title, options, callback) {
+exports.multiselect = function(title, options, ok, cancel, change) {
 	var	dialog = new Packages.android.app.AlertDialog.Builder(Activity)
 	dialog.setTitle(title);
-	dialog.setMultiChoiceItems(options, null, function(d, i) { callback(d, i) });
+	dialog.setMultiChoiceItems(options, null, function(d, i) { change(d, i) });
+	dialog.setPositiveButton('Ok', ok);
+    dialog.setNegativeButton('Cancel', function(d) {
+		cancel();
+		d.dismiss();
+	});
 	dialog.show();
 }
 
-
-
-
 // for pickin dates
-exports.datePicker = function() {
-
+exports.date = function(callback, y, m, d) {
+	var c = java.util.Calendar.getInstance()
+	  , y = c.get(y || java.util.Calendar.YEAR)
+	  , m = c.get(m || java.util.Calendar.MONTH)
+	  , d = c.get(d || java.util.Calendar.DAY_OF_MONTH)
+	  , p = new Packages.android.app.DatePickerDialog(Activity, callback, y, m, d)
+	p.show();
 }
 
 // for pickin times
-exports.timePicker = function() {
-
+exports.time = function(callback, h, m) {
+	var c = java.util.Calendar.getInstance()
+	  , h = c.get(h || java.util.Calendar.HOUR_OF_DAY)
+	  , m = c.get(m || java.util.Calendar.MINUTE)
+	  , p = new Packages.android.app.TimePickerDialog(Activity, callback, h, m, false)
+	p.show();
 }
+
+
+// TODO custom dialgoue, more options for progress (determinate, centered no message, title?)
 
 // indeterminate or determinate progress indicators
 exports.progress = function(msg, fn, win, fail) {
 	var p = Packages.android.app.ProgressDialog.show(Activity, "", msg || "", true);
-
-	new java.Thread(function() {
+	// this is some java-ass javascript
+	new java.lang.Thread(function() {
+		Packages.android.os.Looper.prepare();
 		try {
 			fn();
 			win();
-		} catch(e) {
+			p.dismiss();
+		} 
+		catch(e) {
 			fail();
 		}
-		Activity.runOnUiThread(p.dismiss);
+		finally {
+            Packages.android.os.Looper.loop();
+            Packages.android.os.Looper.myLooper().quit();
+		}
     }).start();
 }
--- src/assets/app.js
+++ src/assets/app.js
@@ -1,5 +1,5 @@
 function onCreate(bundle) {
-	console.log('awwww yeah');
+	console.log('AndroidJS Example Activity');
 	
 	var notification = require('androidjs/notification')
 	  , toast 		 = notification.toast
@@ -7,20 +7,39 @@ function onCreate(bundle) {
 	  , alert 		 = dialogue.alert
 	  , confirm 	 = dialogue.confirm
 	  , select 	 	 = dialogue.select
-	  , selectOne	 = dialogue.selectOne
-	  , selectMany	 = dialogue.selectMany
+	  , multiselect	 = dialogue.multiselect
+	  , date		 = dialogue.date
+	  , time		 = dialogue.time 
+	  , progress     = dialogue.progress
 	
 	toast("Welcome to AndroidJS!");
-	
+	/*
 	confirm('load web browser?'
 	, 		function(){ activity('droidscript/webapp')}
 	,		function(){ alert('no worries')}
 	);
 	
 	var options = ['one', 'two', 'three'];
-	selectMany('pick some'
+	
+	multiselect('pick some'
 	,	   options
-	,      function(d, i){ toast(options[i]) }
+	,      function ok() {}
+	,	   function cancel() {}
+	,      function change(d, i) { toast(options[i]) }
 	);
+	
+	date(function(p, y, m, d) {
+		toast((new Date(y, m, d)).toString());
+	});
+	
+	time(function(p, h, m) {
+		toast('hour: ' + h + ' minute: ' + m);
+	});	
+	*/
+	progress('long running task happening!'
+	,		 function task()    { java.lang.Thread.currentThread().sleep(3000) }
+	,		 function success() { toast('we are done!') }
+	,		 function failure() { toast('task fucked up') }
+	)
 }
 
