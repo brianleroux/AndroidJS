c187d58a0254507645caa0a263987f013395c922
brianleroux <brian@westcoastlogic.com>
2010-10-17 14:31:38 -0700

    borken build

--- src/assets/androidjs/notification.js
+++ src/assets/androidjs/notification.js
@@ -4,6 +4,7 @@ var Toast = Packages.android.widget.Toast;
 exports.toast = function(msg) {		
 	Toast.makeText(Activity, msg, Toast.LENGTH_SHORT).show();
 }
+
 exports.notify = function(id, title, text, msg) {
 	var icon   = R.drawable.notification_icon   // icon from resources
 	  , when   = System.currentTimeMillis() 	// notification time
--- src/src/com/androidjs/example/androidjsexample.java
+++ src/src/com/androidjs/example/androidjsexample.java
@@ -11,7 +11,7 @@ public class androidjsexample extends DroidScriptActivity
     public void onCreate(Bundle savedInstanceState)
     {
 		Intent intent = getIntent();
-		intent.putExtra("ScriptAsset", "android.js");
+		intent.putExtra("ScriptAsset", "androidjs/android.js");
 		super.onCreate(savedInstanceState);
     }
 }
